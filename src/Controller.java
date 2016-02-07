9553839a139f4ed8ada0db482a05314843417c13
Mufhim Anees <mufhimanees@gmail.com>
2016-02-07 08:47:20 +0800

    Rearranged preprocessing sequence to decreasing processing time

--- src/Controller.java
+++ src/Controller.java
@@ -8,141 +8,141 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by Muffff on 1/31/16.
+ * ****************** TESTING THE VERSION CONTROL!!!!
+ */
+public class Controller {
+
+    List<Mat> featureList;
+
+    HOGDescriptor hog;
+    /**
+     * HOG Parameters
+     */
+    Size windowSize;
+    Size cellSize;
+    Size blockSize;
+    Size winStride;
+    Size padding;
+    Size blockStride;
+    int nBins;
+
+
+    public Controller() {
+        initializeHOG();
+    }
+
 
     /**
-     * Created by Muffff on 1/31/16.
+     * Get all JPG file names from given directory.
+     *
+     * @param dir path to directory
+     * @return list of all PNG file names
      */
-    public class Controller {
 
-        List<Mat> featureList;
+    public List<String> getFilenames(String dir) throws IOException {
 
-        HOGDescriptor hog;
-        /**
-         * HOG Parameters
-         */
-        Size windowSize;
-        Size cellSize;
-        Size blockSize;
-        Size winStride;
-        Size padding;
-        Size blockStride;
-        int nBins;
+        List<String> fileNames = new ArrayList<>();
 
+        File[] files = new File(dir).listFiles();
 
-        public Controller() {
-            initializeHOG();
-        }
+        String filePath = null;
 
+        System.out.println("Directory: " + dir);
 
-        /**
-         * Get all JPG file names from given directory.
-         *
-         * @param dir path to directory
-         * @return list of all PNG file names
-         */
+        if (files != null) {
 
-        public List<String> getFilenames(String dir) throws IOException {
-            List<String> fileNames = new ArrayList<>();
-
-            File[] files = new File(dir).listFiles();
-
-            String filePath = null;
-
-            System.out.println("Directory: " + dir);
-
-            if (files != null) {
-
-                for (File file : files) {
-                    if (file.isFile() && file.getName().toLowerCase().endsWith(".jpg")) {
-                        filePath = file.getCanonicalPath();
-                        fileNames.add(filePath);
-                    }
-
+            for (File file : files) {
+                if (file.isFile() && file.getName().toLowerCase().endsWith(".jpg")) {
+                    filePath = file.getName();
+                    fileNames.add(filePath);
                 }
-            } else System.out.println("Empty directory");
 
-            return fileNames;
-        }
+            }
+        } else System.out.println("Empty directory");
 
-        public Mat createModel(String dir) throws IOException {
+        return fileNames;
+    }
 
-            //32 bit Floating Point with 1 channel
-            Mat data = new Mat(0, 0, CvType.CV_32FC1);
+    public Mat createModel(String dir) throws IOException {
 
-            Mat labels = new Mat(0, 0, CvType.CV_32FC1);
+        //32 bit Floating Point with 1 channel
+        Mat data = new Mat(0, 0, CvType.CV_32FC1);
+
+        Mat labels = new Mat(0, 0, CvType.CV_32FC1);
 
 //        featureList = new ArrayList<>();
 
-            MatOfFloat descriptor = new MatOfFloat();
+        MatOfFloat descriptor = new MatOfFloat();
 
-            MatOfPoint locations = new MatOfPoint();
+        MatOfPoint locations = new MatOfPoint();
 
-            List<String> listOfFiles = getFilenames(dir);
+        List<String> listOfFiles = getFilenames(dir);
 
-            for (String file : listOfFiles) {
+        for (String file : listOfFiles) {
 
-                if (file != null) {
-                    Mat img_raw = Imgcodecs.imread(file);
-                    if (img_raw.empty()) {
-                        System.out.println("Image loaded is null");
-                        break;
-                    }
+            if (file != null) {
+                Mat img_raw = Imgcodecs.imread(dir + file);
+                if (img_raw.empty()) {
+                    System.out.println("Image loaded is null");
+                    break;
+                }
 
-                    Mat img_gray = launchPreProcessor(img_raw);
-                    hog.compute(img_gray, descriptor, winStride, padding, locations);
-                    float label = Float.parseFloat(file.substring(38, 39));
-                    Mat l = new Mat(1, 1, CvType.CV_32FC1, new Scalar(label));
-                    labels.push_back(l);
+                Mat img_processed = launchPreProcessor(img_raw);
+                hog.compute(img_processed, descriptor, winStride, padding, locations);
+                float label = Float.parseFloat(file.substring(0, 1));
+                Mat l = new Mat(1, 1, CvType.CV_32FC1, new Scalar(label));
+                labels.push_back(l);
 
-                    data.push_back(descriptor.clone());
-
-                    System.out.println("** done ** \n" + file + " has been computed");
-                } else
-                    System.out.println("NULL FILE");
-            }
-
-            return data;
-
+                data.push_back(descriptor.clone());
+                System.out.println("\nImage size: " + img_processed.size());
+                System.out.println("** done ** \n" + file + " has been computed, label: " + label);
+            } else
+                System.out.println("NULL FILE");
         }
 
-        protected Mat launchPreProcessor(Mat img_raw) {
-            //640,480
-            Size imageResize = new Size(128, 64);
+        return data;
 
-            Mat img_gray = new Mat();
+    }
 
-            Mat img_resized = new Mat();
+    protected Mat launchPreProcessor(Mat img_raw) {
+        //640,480
+        Size imageResize = new Size(128, 64);
 
-            Imgproc.resize(img_raw, img_resized, imageResize);
+        Mat img_gray = new Mat();
 
-            Imgproc.cvtColor(img_raw, img_gray, Imgproc.COLOR_BGR2GRAY);
+        Mat img_resized = new Mat();
 
+        Imgproc.cvtColor(img_raw, img_gray, Imgproc.COLOR_BGR2GRAY);
 
-            return img_gray;
+        Imgproc.resize(img_gray, img_resized, imageResize);
+
+        return img_resized;
 //        displayImage(img_gray);
-        }
+    }
 
 
-        protected void initializeHOG() {
+    protected void initializeHOG() {
 
-            windowSize = new Size(128, 64);
+        windowSize = new Size(128, 64);
 
-            cellSize = new Size(8, 8);
+        cellSize = new Size(8, 8);
 
-            blockSize = new Size(16, 16);
+        blockSize = new Size(16, 16);
 
-            winStride = new Size(windowSize.width / 2, windowSize.height / 2);
+        winStride = new Size(windowSize.width / 2, windowSize.height / 2);
 
-            padding = new Size(0, 0);
+        padding = new Size(0, 0);
 
-            //50% overlap
-            blockStride = new Size(blockSize.width / 2, blockSize.height / 2);
+        //50% overlap
+        blockStride = new Size(blockSize.width / 2, blockSize.height / 2);
 
-            nBins = 9;
+        nBins = 9;
 
-            hog = new HOGDescriptor(windowSize, blockSize, blockStride, cellSize, nBins);
+        hog = new HOGDescriptor(windowSize, blockSize, blockStride, cellSize, nBins);
 
-        }
+    }
 
 //    protected void computeHOG()
 //    {
@@ -161,5 +161,4 @@ import java.util.List;
 ////        write(descriptor);
 //
 //    }
-
-    }
+}
